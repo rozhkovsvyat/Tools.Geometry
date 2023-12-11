bf6085f47698326ac6b3460c30a0c6c4804a079b
Svyat Rozhkov <rozhkovsvyat@gmail.com>
2023-12-11 10:40:10 +0700

    Add Circle and Triangle args

---
+++ Geometry/ShapeArgs/CircleArgs.cs
@@ -0,0 +1,27 @@
+﻿namespace Tools.Geometry;
+
+/// <summary>
+/// Contains <see cref="Circle"/> constructor arguments.
+/// </summary>
+public class CircleArgs : IShapeArgs
+{
+	#region IShapeArgs
+
+	/// <inheritdoc/>
+	public static Recipe<IShape, IArgs> Recipe 
+		=> a => a is not CircleArgs args ? 
+			new DefaultShape() : new Circle(args.Radius);
+
+	#endregion
+
+	/// <summary>
+	/// Radius.
+	/// </summary>
+	public double Radius { get; }
+
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	/// <param name="radius">Circle radius.</param>
+	public CircleArgs(double radius) => Radius = radius;
+}
---
+++ Geometry/ShapeArgs/TriangleArgs.cs
@@ -0,0 +1,42 @@
+﻿namespace Tools.Geometry;
+
+/// <summary>
+/// Contains <see cref="Triangle"/> constructor arguments.
+/// </summary>
+public class TriangleArgs : IShapeArgs
+{
+	#region IShapeArgs
+
+	/// <inheritdoc/>
+	public static Recipe<IShape, IArgs> Recipe 
+		=> a => a is not TriangleArgs args ? new DefaultShape() 
+			: new Triangle(args.SideA, args.SideB, args.SideC);
+
+	#endregion
+
+	/// <summary>
+	/// Side A.
+	/// </summary>
+	public double SideA { get; }
+	/// <summary>
+	/// Side B.
+	/// </summary>
+	public double SideB { get; }
+	/// <summary>
+	/// Side C.
+	/// </summary>
+	public double SideC { get; }
+
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	/// <param name="sideA">Triangle side A.</param>
+	/// <param name="sideB">Triangle side B.</param>
+	/// <param name="sideC">Triangle side C.</param>
+	public TriangleArgs(double sideA, double sideB, double sideC)
+	{
+		SideA = sideA;
+		SideB = sideB;
+		SideC = sideC;
+	}
+}
