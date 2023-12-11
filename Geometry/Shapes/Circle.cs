799de24640be0521481e45c72b3f78f7a4cb6452
Svyat Rozhkov <rozhkovsvyat@gmail.com>
2023-12-11 10:38:47 +0700

    Add circle class

---
+++ Geometry/Shapes/Circle.cs
@@ -0,0 +1,55 @@
+﻿namespace Tools.Geometry;
+
+/// <summary>
+/// Contains circle properties and methods.
+/// </summary>
+public class Circle : IShape
+{
+	#region Static
+
+	/// <summary>
+	/// Returns the area of a circle.
+	/// </summary>
+	/// <param name="radius">Radius.</param>
+	/// <returns>Area result.</returns>
+	public static double GetArea(double radius) 
+		=> Math.PI * Math.Pow(radius, 2);
+
+	/// <summary>
+	/// Returns the perimeter of a circle.
+	/// </summary>
+	/// <param name="radius">Radius.</param>
+	/// <returns>Perimeter result.</returns>
+	public static double GetPerimeter(double radius) 
+		=> 2 * Math.PI * radius;
+
+	#endregion
+
+	#region IShape
+
+	/// <inheritdoc/>
+	public double Perimeter 
+		=> _perimeter ??= GetPerimeter(_radius);
+	private double? _perimeter;
+
+	/// <inheritdoc/>
+	public double Area 
+		=> _area ??= GetArea(_radius);
+	private double? _area;
+
+	#endregion
+
+	/// <summary>
+	/// Circle radius.
+	/// </summary>
+	private readonly double _radius;
+
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	/// <param name="radius">Circle radius.</param>
+	/// <exception cref="ArgumentException"></exception>
+	internal Circle(double radius) 
+		=> _radius = radius > 0 ? radius : throw new ArgumentException
+			($"Input {nameof(radius)} is less than or equal to zero.");
+}
