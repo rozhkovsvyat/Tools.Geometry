ee39c05cc76be938815fd102b79ad0a283e105b6
Svyat Rozhkov <rozhkovsvyat@gmail.com>
2023-12-11 10:39:44 +0700

    Add ShapeArgs interface

---
+++ Geometry/ShapeArgs/IShapeArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace Tools.Geometry;
+
+/// <summary>
+/// Provides constructor arguments.
+/// </summary>
+public interface IArgs { }
+
+/// <inheritdoc cref="IArgs"/>
+/// <remarks>Provides a static <see cref="IShape"/> recipe (function that returns a new <see cref="IShape"/>).</remarks>
+public interface IShapeArgs : IArgs
+{
+	/// <summary>
+	/// Static <see cref="IShape"/> recipe.
+	/// </summary>
+	/// <returns>Function that returns a new <see cref="IShape"/>.</returns>
+	public static abstract Recipe<IShape, IArgs> Recipe { get; }
+}
